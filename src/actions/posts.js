a156d994a3ece5c4ace8abb8d81cdcef62f78166
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-19 22:04:24 -0800

    Started on posts actions, so far made addPost and getPosts functions.

---
+++ src/actions/posts.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+import { GET_POSTS, ADD_POST } from "./types";
+
+export const addPost =
+  ({ text }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({ text });
+
+    try {
+      const res = await axios.post("/api/posts", body, config);
+
+      dispatch({
+        type: ADD_POST,
+        payload: res.data,
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+export const getPosts = () => (dispatch) => {
+  try {
+    const res = await axios.get("/api/posts");
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
