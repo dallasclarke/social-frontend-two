46c8db91620c556c81e77097a6165f4f22985282
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-20 23:20:32 -0800

    PostForm is successfully working, able to write post and have it show up on feed.

--- src/components/postForm/PostForm.jsx
+++ src/components/postForm/PostForm.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { addPost } from "../../actions/posts";
 
 import "./PostForm.css";
 
 function PostForm() {
-    const [text, setText] = useState("")
+  const [text, setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addPost({ text }));
+    setText("");
+  };
 
   return (
     <div className="post-form">
-      <form>
-        <textarea />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <textarea
+          name="text"
+          value={text}
+          onChange={(e) => handleChange(e)}
+          required
+        />
         <input type="submit" />
       </form>
     </div>
