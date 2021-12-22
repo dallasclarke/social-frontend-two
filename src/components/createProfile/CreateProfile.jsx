246eaf526dcf9bffda2cc64551f4c179954d7e4b
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-22 00:38:25 -0800

    CreateProfile component skeleton mostly done

--- src/components/createProfile/CreateProfile.jsx
+++ src/components/createProfile/CreateProfile.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { createProfile } from "../../actions/profile";
 
 function CreateProfile() {
-    return (
-        <div>
-            Create Profile
-        </div>
-    )
+  const [formDate, setFormData] = useState({
+    bio: "",
+    city: "",
+    state: "",
+  });
+
+  const { bio, city, state } = formData;
+
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="create-profile">
+      <h1>Lets create your new profile!</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <label>Bio:</label>
+        <textarea />
+        <label>City:</label>
+        <input type="text" />
+        <label>State/Province</label>
+        <input type="text" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
-export default CreateProfile
+export default CreateProfile;
