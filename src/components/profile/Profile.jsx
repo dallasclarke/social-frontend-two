91d7821bd6b27d499580112b0c6ce3ac11f5c20f
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2022-01-02 20:31:12 -0800

    On profile component post to that specific user will be displayed on their profile.

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -5,10 +5,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 
 import About from "../about/About";
+import Post from "../post/Post";
 
 function Profile() {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user._id);
   const profile = useSelector((state) => state.profile);
+  const posts = useSelector((state) => state.posts.posts);
 
   useEffect(() => {
     dispatch(getCurrentProfile())
@@ -16,6 +19,9 @@ function Profile() {
 
   const userProfile = profile.profile;
   const loading = profile.loading;
+  const usersPosts = posts.filter((post) => post.user === user);
+
+  console.log(usersPosts);
 
   return loading && userProfile === null ? (
     <>
@@ -24,8 +30,16 @@ function Profile() {
   ) : (
     <div className="profile">
       <About profile={userProfile} />
+      {usersPosts.map((post) => (
+        <Post
+          key={post._id}
+          name={post.name}
+          date={post.date}
+          text={post.text}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
 export default Profile;
